672fc7387bff991411897be857542c0861e6317e
cclintris <181250083@smail.nju.edu.cn>
2021-12-28 12:57:27 +0800

    add: update check version function

---
+++ generators/app/constant.js
@@ -0,0 +1,17 @@
+const APP_TYPE = {
+  WEBPACK: 'webpack',
+  ROLLUP: 'rollup',
+}
+
+const BOXEN_OPTS = {
+  padding: 1,
+  margin: 1,
+  align: 'center',
+  borderColor: 'yellow',
+  borderStyle: 'round',
+}
+
+module.exports = {
+  APP_TYPE,
+  BOXEN_OPTS,
+}
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,72 @@
 const Generator = require('yeoman-generator')
+const updateNotifier = require('update-notifier')
+const chalk = require('chalk')
+const boxen = require('boxen')
+const beeper = require('beeper')
 
-class WebRollGenerator extends Generator {}
+const { APP_TYPE, BOXEN_OPTS } = require('./constant')
+const pkg = require('./package')
+
+class WebRollGenerator extends Generator {
+  constructor(args, opts) {
+    super(args, opts)
+
+    this.type = APP_TYPE.WEBPACK
+
+    this._getDefaultDir = this._getDefaultDir.bind(this)
+  }
+
+  _getDefaultDir() {
+    return `${this.type}-app`
+  }
+
+  /**
+   * check version of Generator-Create-WebRoll CLI dependencies
+   * @return boolean
+   */
+  _checkVersion() {
+    this.log()
+    this.log('🛠️  Checking your Generator-Create-WebRoll CLI version...')
+
+    let checkResult = false
+
+    const notifier = updateNotifier({
+      pkg,
+      updateCheckInterval: 0,
+    })
+
+    const update = notifier.update
+    if (update) {
+      const messages = []
+      const { current, latest } = update
+      messages.push(
+        chalk.bgYellow.black(' WARNING: ') +
+          '  Generator-Create-WebRoll is not latest.\n'
+      )
+      messages.push(
+        chalk.grey('current: ') +
+          chalk.grey(current) +
+          chalk.grey(' → ') +
+          chalk.grey('latest: ') +
+          chalk.green(latest)
+      )
+      messages.push(chalk.grey('Up to date ') + `npm i -g ${pkg.name}`)
+      this.log(boxen(messages.join('\n'), BOXEN_OPTS))
+      beeper()
+      this.log(
+        '🛠️  Finish checking your Generator-Create-WebRoll CLI. CAUTION ↑↑',
+        '⚠️'
+      )
+    } else {
+      checkResult = true
+      this.log(
+        '🛠️  Done checking your Generator-Create-WebRoll CLI. Good to go!',
+        chalk.green('✔')
+      )
+    }
+
+    return checkResult
+  }
+}
 
 module.exports = WebRollGenerator
